1fa8626fa1cb0245ac6be650b8227402b0a79554
Mostafa Ayman <mostafaaboelmagd01@gmail.com>
2025-08-25 12:48:06 +0300

    create basic users module and connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    MongooseModule.forRoot(
+      'mongodb+srv://dbAdmin:vzYiWLfYP7QQ0hK9@familyfeaturedb.kkbgx5x.mongodb.net/?retryWrites=true&w=majority&appName=familyFeatureDB',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
