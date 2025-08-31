109fd28c58e31d7866b0a70b0688006e407d5755
Mostafa Ayman <mostafaaboelmagd01@gmail.com>
2025-08-31 12:16:24 +0300

    Added ENV file for database credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -6,9 +7,17 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://test_admin:O5lV6vNUBlOkJBGF@familyfeaturedb.kkbgx5x.mongodb.net/?retryWrites=true&w=majority&appName=familyFeatureDB',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService available everywhere
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // Not strictly needed if isGlobal: true, but good practice
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get('DATABASE_ADMIN')}:${configService.get('DATABASE_PASSWORD')}@familyfeaturedb.kkbgx5x.mongodb.net/?retryWrites=true&w=majority&appName=familyFeatureDB`,
+      }),
+    }),
     UsersModule,
   ],
   controllers: [AppController],
